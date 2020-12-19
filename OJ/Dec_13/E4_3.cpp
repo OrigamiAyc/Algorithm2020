dfb4fcb6bd0d0e369350957976b70a0cb7e031dd
Lapland Stark <L.G.Stark@outlook.com>
2020-12-19 17:20:48 +0800

    E4-3

--- OJ/Dec_13/E4_3.cpp
+++ OJ/Dec_13/E4_3.cpp
@@ -1,10 +1,10 @@
 /**
  * At first I meant to change the lifespan of a number when it's shot in the cache
- * so I chose min-heap instead of priority queue to store those lifespans
+ * so I chose max-heap instead of priority queue to store those lifespans
  * since queues cannot be random accessed
  * However it's hard to posite that element in a dynamic set
  * thus I do not pop out when it should be changed out
- * for the old data should be
+ * for the old data should never be heap top
  **/
 #include <iostream>
 #include <vector>
@@ -12,21 +12,38 @@
 #include <algorithm>
 using namespace std;
 
+#define INF 2147483647
+
 struct cacheItem
 {
-	char num;
 	int lifespan;
+	int num;
+	bool operator<(cacheItem a)const
+	{
+		return lifespan < a.lifespan;
+	}
 };
 
 /**
  * 10000 is the memory limit
- * in each cell stores the
+ * I didn't mem the next position since I used the string func
  **/
-int ifCached[10000];
+bool ifCached[10001];
+
+class less_class
+{
+public:
+	bool operator()(cacheItem a, cacheItem b)
+	{
+		return a.lifespan < b.lifespan;
+	}
+};
 
 int main(int argc, char const *argv[])
 {
 	int n, k;
+	cin >> n >> k;
+	memset(ifCached, 0, sizeof(ifCached));
 	char *input = new char[n];
 	int *inputInt = new int[n];
 	vector<cacheItem> buffer;
@@ -39,17 +56,59 @@ int main(int argc, char const *argv[])
 		input[i] = (char)inputInt[i];
 	}
 	string inputStrem(input);
+	/** NOTE:
+	 * Cannot use buffer.size() directly
+	 * since we've pushed item inside buffer when is already in there
+	 * the buffer.size() can reach k even if there have not been k different items
+	 **/
+	// int bufferSize = 0;
 
 	int missTimes = 0;
 	for (size_t i = 0; i < n; i++)
 	{
-		int nextpos = inputStrem.find_first_of(input[i]);
+		int nextpos = inputStrem.find(input[i], i + 1);
+		// cout << "firstof " << inputInt[i] << " is " << nextpos << endl;
 		if (ifCached[inputInt[i]])
+		/**
+		 * push a new item with its arguments instead of changing the original element
+		 * This will not have an impact on the heap
+		 * since what we kick out is always the one with the largest lifespan
+		 * the small (actually already visited) time num does not matter
+		 **/
 		{
-			/* code */
+			int lifespan = (nextpos == -1) ? INF : nextpos;
+			buffer.push_back((cacheItem){lifespan, inputInt[i]});
+			push_heap(buffer.begin(), buffer.end());
+			// OR we can just let k soar
+			k++;
 		}
-
+		else if (/* bufferSize */ buffer.size() < k)
+		// The buffer is not full yet
+		{
+			missTimes++;
+			int lifespan = (nextpos == -1) ? INF : nextpos;
+			buffer.push_back((cacheItem){lifespan, inputInt[i]});
+			push_heap(buffer.begin(), buffer.end());
+			ifCached[inputInt[i]] = true;
+			// bufferSize++;
+			// cout << "bufferSize = " << bufferSize << endl;
+		}
+		else
+		// Buffer full, need to kick out the heap top
+		{
+			cacheItem heapPop = buffer.front();
+			// cout << "now poped is " << heapPop.num << endl;
+			pop_heap(buffer.begin(), buffer.end());
+			buffer.pop_back();
+			ifCached[heapPop.num] = false;
+			missTimes++;
+			int lifespan = (nextpos == -1) ? INF : nextpos;
+			buffer.push_back((cacheItem){lifespan, inputInt[i]});
+			push_heap(buffer.begin(), buffer.end());
+			ifCached[inputInt[i]] = true;
+		}
+		ifCached[inputInt[i]] = true;
 	}
-
+	cout << missTimes << endl;
 	return 0;
 }
